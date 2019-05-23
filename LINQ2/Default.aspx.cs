d95491366932a42013a25f703998f77d06537f52
Bikash Pokhrel <bpokhrel@aut.ac.nz>
2019-05-23 15:03:06 +1200

    Add project files.

---
+++ LINQ2/Default.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LINQ2
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        List<Student> studentList;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            studentList = new List<Student>()
+            {
+                new Student{Id = 101, Name = "Anil", Cost = 7000, Course = "Web Design"},
+                new Student{Id = 102, Name = "Arshdeep", Cost = 8000, Course = "Computer Science"},
+                new Student{Id = 103, Name = "Harman", Cost = 9000, Course = "Networking"},
+                new Student{Id = 104, Name = "Bikram", Cost = 10000, Course = "ICT"},
+                new Student{Id = 105, Name = "Gagan", Cost = 7000, Course = "Web Design"},
+                new Student{Id = 106, Name = "Love", Cost = 15000, Course = "Business"},
+                new Student{Id = 107, Name = "Jashan", Cost = 9500, Course = "MBA"},
+                new Student{Id = 108, Name = "Jeevan", Cost = 6000, Course = "Hardware Design"}
+            };
+            var students = from student in studentList
+                           where student.Cost<=7000 && student.Course == "Web Design"
+                select student;
+            GVStudents.DataSource = students;
+            GVStudents.DataBind();
+
+        }
+    }
+}
---
+++ LINQ2/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LINQ2
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Cost { get; set; }
+        public string Course { get; set; }
+
+    }
+}
